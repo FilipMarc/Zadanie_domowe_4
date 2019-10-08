f6cdc5aef4204047a43349f3c58446f40f1a6686
Filip Marc <Filipmarc71@gmail.com>
2019-10-08 13:47:29 +0200

    Adding other solution to RightTriangleClass.

--- src/rectualTriangle/RightTriangleTest.java
+++ src/rectualTriangle/RightTriangleTest.java
@@ -2,7 +2,7 @@ package rectualTriangle;
 
 public class RightTriangleTest {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(4,6,8);
+        Triangle triangle = new Triangle(3,4,5);
         RightTriangle rightTriangle = new RightTriangle();
         boolean rectualTriangle = rightTriangle.isRightTriangle(triangle);
 
